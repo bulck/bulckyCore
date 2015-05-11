e0174172c4d84bdf548e97d94ad0ab99daa4f09e
alliaume.rico <alliaume.rico@gmail.com>
2015-05-11 10:54:11 +0200

    Correction de l'appel dans l'exemple

--- serverPlugUpdate/serverPlugUpdate.tcl
+++ serverPlugUpdate/serverPlugUpdate.tcl
@@ -135,4 +135,4 @@ emeteur_subscriptionEvenement
 vwait forever
 
 # tclsh "D:\CBX\cultipiCore\serverPlugUpdate\serverPlugUpdate.tcl" "D:\CBX\cultipiCore\serverPlugUpdate\confExample\conf.xml"
-# tclsh /opt/cultipi/serverPlugUpdate/serverPlugUpdate.tcl 6004 /etc/cultipi/01_defaultConf_RPi/./serverPlugUpdate/conf.xml 6003 6000
+# tclsh /opt/cultipi/serverPlugUpdate/serverPlugUpdate.tcl /etc/cultipi/01_defaultConf_RPi/./serverPlugUpdate/conf.xml
