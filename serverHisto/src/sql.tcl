524e28f56faebe9091f0af83039348ded3bcdea2
alliaume.rico <alliaume.rico@gmail.com>
2015-05-20 20:16:17 +0200

    ENcore 1 !

--- serverHisto/src/sql.tcl
+++ serverHisto/src/sql.tcl
@@ -79,7 +79,7 @@ proc ::sql::AddSensorValue {sensor val1 val2 time} {
         set val2 [string map {" " "0"} [format %4.f [expr $val2 * 100]]]
     }
     
-    if {$val1 != NULL ||$val2 != NULL } {
+    if {$val1 != NULL || $val2 != NULL } {
         ::sql::query "INSERT INTO logs (timestamp, record1, record2, date_catch, time_catch, fake_log, sensor_nb) VALUES ($formattedTime , $val1 , $val2 , \"$date_catch\" , \"$time_catch\" , \"False\" , $sensor);"
     } else {
         ::piLog::log [clock milliseconds] "warning" "::sql::AddSensorValue : val1 $val1 and val2 $val2 are not correct"
