229dfd36467f2d0620edd2e08cfc16663674ce0a
alliaume.rico <alliaume.rico@gmail.com>
2015-06-16 21:25:52 +0200

    AJout du server trigger

---
+++ serverTrigger/serverTrigger.tcl
@@ -0,0 +1,171 @@
+# Pour lancer l'automatisation : tclsh /home/sdf/Bureau/program/progam.tcl
+# Pour arreter : Ctrl + C
+
+# Lecture des arguments : seul le path du fichier XML est donné en argument
+set confXML                 [lindex $argv 0]
+
+set moduleLocalName serverTrigger
+
+# Load lib
+set rootDir [file dirname [file dirname [info script]]]
+
+# Load lib
+lappend auto_path [file join $rootDir lib tcl]
+package require piLog
+package require piServer
+package require piTools
+package require piXML
+
+source [file join $rootDir ${::moduleLocalName} src serveurMessage.tcl]
+
+
+# Initialisation d'un compteur pour les commandes externes envoyées
+set TrameIndex 0
+# On initialise la conf XML
+array set configXML {
+    verbose     debug
+}
+
+if {[file exists $confXML] != 1} {
+    # Le fichier de conf n'exists pas, on meurt tranquillement
+    puts "[clock milliseconds] info ${::moduleLocalName} Conf file ($confXML) does not exists. Bye Bye !"
+    exit
+}
+
+# Chargement de la conf XML
+set RC [catch {
+    array set configXML [::piXML::convertXMLToArray $confXML]
+} msg]
+if {$RC != 0} {
+    puts "[clock milliseconds] info ${::moduleLocalName} [clock milliseconds] error $msg"
+}
+
+# On initialise la connexion avec le server de log
+::piLog::openLog $::piServer::portNumber(serverLog) ${::moduleLocalName} $configXML(verbose)
+
+::piLog::log [clock milliseconds] "info" "starting ${::moduleLocalName} - PID : [pid]"
+::piLog::log [clock milliseconds] "info" "port ${::moduleLocalName} : $::piServer::portNumber(${::moduleLocalName})"
+::piLog::log [clock milliseconds] "info" "confXML : $confXML"
+# On affiche les infos dans le fichier de debug
+foreach element [lsort [array names configXML]] {
+    ::piLog::log [clock milliseconds] "info" "$element : $configXML($element)"
+}
+
+proc bgerror {message} {
+    ::piLog::log [clock milliseconds] error_critic "bgerror in [info script] $::argv -$message- "
+    foreach elem [split $::errorInfo "\n"] {
+        ::piLog::log [clock milliseconds] error_critic " * $elem"
+    }
+}
+
+# Load server
+::piLog::log [clock millisecond] "info" "starting serveur"
+::piServer::start messageGestion $::piServer::portNumber(${::moduleLocalName})
+::piLog::log [clock millisecond] "info" "serveur is started"
+
+proc stopIt {} {
+    ::piLog::log [clock milliseconds] "info" "Start stopping ${::moduleLocalName}"
+    set ::forever 0
+    ::piLog::log [clock milliseconds] "info" "End stopping ${::moduleLocalName}"
+    
+    # Arrêt du server de log
+    ::piLog::closeLog
+    
+    exit
+}
+
+# On commence par prendre un abonnement sur tous les capteurs dont on a besoin
+for {set state 1} {$state <= $configXML(nbEtat)} {incr state} {
+
+    if {$configXML(etat,${state},condition,type) == "sensor"} {
+        
+        set sensorIndex $configXML(etat,2,condition,sensor)
+        
+        # On prend un abonnement
+        set retErr 1
+        while {$retErr != 0} {
+            set retErr 0
+
+            incr retErr [::piServer::sendToServer $::piServer::portNumber(serverAcqSensor) "$::piServer::portNumber(${::moduleLocalName}) [incr ::TrameIndex] subscription ${sensorIndex},value,1 100"]
+            if {$retErr == 0} {
+                incr retErr [::piServer::sendToServer $::piServer::portNumber(serverAcqSensor) "$::piServer::portNumber(${::moduleLocalName}) [incr ::TrameIndex] subscription ${sensorIndex},value,2 100"]
+            }
+            
+            # On initialise la variable
+            set ::sensor(${sensorIndex},value,1) ""
+            set ::sensor(${sensorIndex},value,2) ""
+
+            if {$retErr != 0} {
+                ::piLog::log [clock milliseconds] "warning" "Coudnot take subscription of sensor $sensorIndex "
+                
+                # Si on a pas réussi, on attend un peu
+                after 1000
+            } else {
+                ::piLog::log [clock milliseconds] "debug" "Oh Yeah ! take subscription of sensor $sensorIndex "
+            }
+            after 200
+            update
+        }
+    }
+}
+
+
+
+set actualState 1
+
+while {$actualState <= $configXML(nbEtat)} {
+
+    # On initialise avec le premier état
+    ::piLog::log [clock milliseconds] "info" "Initialisation state $actualState"
+    for {set act 1} {$act <=  $configXML(etat,${actualState},nbAction)} {incr act} {
+        switch $configXML(etat,${actualState},action,${act},type) {
+            "plug" {
+                set priseNumero [lindex $configXML(etat,${actualState},action,${act},value) 0]
+                set priseEtat [lindex $configXML(etat,${actualState},action,${act},value) 1]
+                ::piLog::log [clock milliseconds] "info" "Set plug $priseNumero to $priseEtat"
+                ::piServer::sendToServer $::piServer::portNumber(serverPlugUpdate) "$::piServer::portNumber(${::moduleLocalName}) 0 setRepere $priseNumero $priseEtat 86399" localhost
+            }
+            default {
+                ::piLog::log [clock milliseconds] "error" "Action $configXML(etat,${actualState},action,${act},type) is not recognized"
+            }
+        }
+    }
+
+    # Attente condition
+    switch $configXML(etat,${actualState},condition,type) {
+        "time_ms" {
+            set time $configXML(etat,${actualState},condition,value)
+            ::piLog::log [clock milliseconds] "info" "Wait $time before next action"
+            after $time
+        } 
+        "sensor" {
+            set sensorT $configXML(etat,${actualState},condition,sensor)
+            set neededValue $configXML(etat,${actualState},condition,value)
+            
+            set count 0 
+            while {$::sensor(${sensorT},value,1) != $neededValue} {
+                after 10
+                update
+                incr count
+                if {[expr $count % 100] == 0} {
+                    ::piLog::log [clock milliseconds] "info" "Wait sensor $sensorT to be $neededValue"
+                }
+            }
+        }
+        default {
+            ::piLog::log [clock milliseconds] "error" "Condition $configXML(etat,${actualState},condition,type) is not recognized"
+        }
+    }
+    
+    incr actualState
+
+}
+
+
+update
+
+vwait forever
+
+# Lancement 
+# tclsh D:\CBX\cultipiCore\serverTrigger\serverTrigger.tcl" "D:\CBX\cultipiCore\serverTrigger\confExample\conf.xml" 
+# tclsh /home/sdf/Bureau/cultipiCore/serverIrrigation/serverIrrigation.tcl "/home/sdf/Bureau/cultipiCore/serverIrrigation/confExample/conf.xml" 
---
+++ serverTrigger/src/serveurMessage.tcl
@@ -0,0 +1,101 @@
+
+proc messageGestion {message networkhost} {
+
+    # Trame standard : [FROM] [INDEX] [commande] [argument]
+    set serverForResponse   [::piTools::lindexRobust $message 0]
+    set indexForResponse    [::piTools::lindexRobust $message 1]
+    set commande            [::piTools::lindexRobust $message 2]
+
+    switch ${commande} {
+        "stop" {
+            ::piLog::log [clock milliseconds] "info" "Asked stop"
+            stopIt
+        }
+        "pid" {
+            ::piLog::log [clock milliseconds] "info" "Asked pid"
+            ::piServer::sendToServer $serverForResponse "$::piServer::portNumber(${::moduleLocalName}) $indexForResponse _pid ${::moduleLocalName} [pid]" $networkhost
+        }
+        "getRepere" {
+            # Pour toutes les variables demandées
+            set indexVar 3
+            set returnList ""
+            while {[set variable [::piTools::lindexRobust $message $indexVar]] != ""} {
+                # La variable est le nom de la variable à lire
+                
+                ::piLog::log [clock milliseconds] "debug" "Asked getRepere $variable"
+                
+                if {[info exists ::$variable] == 1} {
+                
+                    eval set returnValue $$variable
+
+                    lappend returnList $returnValue
+                } else {
+                    ::piLog::log [clock milliseconds] "error" "Asked variable $variable - variable doesnot exists"
+                }
+                
+                incr indexVar
+            }
+
+            ::piLog::log [clock milliseconds] "info" "response : $serverForResponse $indexForResponse _getRepere - $returnList - to $networkhost"
+            ::piServer::sendToServer $serverForResponse "$serverForResponse $indexForResponse _getRepere $returnList" $networkhost
+            
+        }
+        "_getPort" {
+            set ::port([::piTools::lindexRobust $message 3]) [::piTools::lindexRobust $message 4]
+            ::piLog::log [clock milliseconds] "debug" "getPort response : module [::piTools::lindexRobust $message 3] port [::piTools::lindexRobust $message 4]"
+        }
+        "_getRepere" {
+            # On parse le retour de la commande
+            set indexCapteur  [::piTools::lindexRobust $message 3]
+            set valeurCapteur [::piTools::lindexRobust $message 4]
+            
+            # On sauvegarde la valeur du capteur
+            set ::sensor(${indexCapteur}) $valeurCapteur
+            
+            ::piLog::log [clock milliseconds] "debug" "getRepere response : capteur $indexCapteur valeur -$valeurCapteur-"
+        }
+        "_subscription" -
+        "_subscriptionEvenement" {
+            # On parse le retour de la commande
+            set variable  [::piTools::lindexRobust $message 3]
+            set valeur [::piTools::lindexRobust $message 4]
+            
+            # On enregistre le retour de l'abonnement
+            set ::${variable} $valeur
+            
+            ::piLog::log [clock milliseconds] "debug" "subscription response : variable $variable valeur -$valeur-"
+        }
+        default {
+            # Si on reçoit le retour d'une commande, le nom du serveur est le notre
+            if {$serverForResponse == $::piServer::portNumber(${::moduleLocalName})} {
+            
+                if {[array names ::TrameSended -exact $indexForResponse] != ""} {
+                    
+                    switch [lindex $::TrameSended($indexForResponse) 0] {
+                        "update_plug_value" {
+                            set plugumber [lindex $::TrameSended($indexForResponse) 1]
+                        
+                            set ::plug($plugumber,updateStatus) $commande
+                            set ::plug($plugumber,updateStatusComment) ${message}
+                        
+                            ::piLog::log [clock milliseconds] "info" "I2C Update plug $plugumber updateStatus : -$commande- updateStatusComment : -${message}-"
+                        
+                            # On supprime cette donnée de la mémoire
+                            unset ::TrameSended($indexForResponse)
+                        }
+                        default {
+                            ::piLog::log [clock milliseconds] "error" "Not recognize keyword response -${message}-"
+                        }                    
+                    }
+                    
+                } else {
+                    ::piLog::log [clock milliseconds] "error" "Not requested response -${message}-"
+                }
+            
+                
+            } else {
+                ::piLog::log [clock milliseconds] "error" "Received -${message}- but not interpreted"
+            }
+        }
+    }
+}
