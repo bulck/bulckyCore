a0a676632c9c774fa09d4d5d7fe54435e526688b
alliaume.rico <alliaume.rico@gmail.com>
2016-04-15 16:14:32 +0200

    Correction des chemins

--- bulckyPi/setCommand.tcl
+++ bulckyPi/setCommand.tcl
@@ -23,11 +23,11 @@ set adresseIP [lindex $argv 1]
 ::piServer::sendToServer $::piServer::portNumber($module) "$::piServer::portNumber(serverSetCommand) 0 [lrange $argv 2 [expr $argc - 1]]" $adresseIP
 
 
-# tclsh /opt/cultipi/cultiPi/setCommand.tcl serverPlugUpdate localhost setRepere 1 on 10
-# tclsh "D:\CBX\06_bulckyCore\cultiPi\setCommand.tcl" serverPlugUpdate localhost setRepere 1 on 10
-# tclsh /opt/cultipi/cultiPi/setCommand.tcl serverMail localhost sendMail info@greenbox-botanic.com "Essai 2 " "Corps du message et contenu"
-# tclsh "D:\CBX\06_bulckyCore\cultiPi\setCommand.tcl" serverMail localhost sendMail gl@greenbox-botanic.com "Essai 2 " "Corps du message et contenu"
-# tclsh "D:\CBX\06_bulckyCore\cultiPi/setCommand.tcl" serverIrrigation localhost stop
-# tclsh "D:\CBX\06_bulckyCore\cultiPi/setCommand.tcl" serverIrrigation localhost fillCuve
-# tclsh /opt/cultipi/cultiPi/setCommand.tcl serverMail localhost sendMail info@cultibox.fr test Body
-# tclsh /opt/cultipi/cultiPi/getCommand.tcl serverIrrigation localhost gsetRepere "::regulationActivePlateforme(2)" false
+# tclsh /opt/bulckypi/bulckyPi/setCommand.tcl serverPlugUpdate localhost setRepere 1 on 10
+# tclsh "D:\CBX\06_bulckyCore\bulckyPi\setCommand.tcl" serverPlugUpdate localhost setRepere 1 on 10
+# tclsh /opt/bulckypi/bulckyPi/setCommand.tcl serverMail localhost sendMail info@greenbox-botanic.com "Essai 2 " "Corps du message et contenu"
+# tclsh "D:\CBX\06_bulckyCore\bulckyPi\setCommand.tcl" serverMail localhost sendMail gl@greenbox-botanic.com "Essai 2 " "Corps du message et contenu"
+# tclsh "D:\CBX\06_bulckyCore\bulckyPi/setCommand.tcl" serverIrrigation localhost stop
+# tclsh "D:\CBX\06_bulckyCore\bulckyPi/setCommand.tcl" serverIrrigation localhost fillCuve
+# tclsh /opt/bulckypi/bulckyPi/setCommand.tcl serverMail localhost sendMail info@cultibox.fr test Body
+# tclsh /opt/bulckypi/bulckyPi/getCommand.tcl serverIrrigation localhost setRepere "::regulationActivePlateforme(2)" false
